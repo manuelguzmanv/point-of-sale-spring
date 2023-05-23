6667777feb8a8e7ef25e91df69ada22066573711
ManuelG <manuelguzmanvilches@gmail.com>
2023-05-22 22:09:47 -0400

    [ADD] employee model

---
+++ src/main/java/com/mgdev/model/Employe.java
@@ -0,0 +1,74 @@
+package com.mgdev.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employe")
+public class Employe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
+	@Column(name = "last_name", length = 30)
+	private String lastName; 
+	
+	@Column(name = "first_name", length = 30)
+	private String firstName;
+	
+	@Column(name = "position", length = 30)
+	private String position;
+	
+	@Column(name = "photo_url", length = 50)
+	private String photoUrl;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	
+	
+	
+	
+
+}
