1bf59a9f4bb5648ad746be59c504df0fd900cfdd
ManuelG <manuelguzmanvilches@gmail.com>
2023-05-22 21:45:27 -0400

    [ADD] model 'supplier'

---
+++ src/main/java/com/mgdev/model/Supplier.java
@@ -0,0 +1,131 @@
+package com.mgdev.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="supplier")
+public class Supplier {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
+	@Column(name = "company_name", length = 50)
+	private String companyName; 
+	
+	@Column(name = "contact_name", length = 30)
+	private String contactName; 
+	
+	@Column(name = "adress", length = 30)
+	private String adress; 
+	
+	@Column(name = "city", length = 20)
+	private String city;
+	
+	@Column(name = "region", length = 20)
+	private String region;
+	
+	@Column(name = "country", length = 20)
+	private String country;
+	
+	//validation
+	//@Email
+	@Column(name = "email", length = 30)
+	private String email;  
+	
+	@Column(name = "phone", length = 15)
+	private String phone;
+	
+	@Column(name = "website", length = 20)
+	private String website;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getAdress() {
+		return adress;
+	}
+
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	
+	
+	
+	
+	
+}
