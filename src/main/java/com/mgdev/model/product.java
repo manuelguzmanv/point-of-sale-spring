3996762e0967340c2d6fab70b5969f6913777b02
ManuelG <manuelguzmanvilches@gmail.com>
2023-05-25 21:37:54 -0400

    [ADD] product model

---
+++ src/main/java/com/mgdev/model/product.java
@@ -0,0 +1,86 @@
+package com.mgdev.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "product_name", length = 30)
+	private String productName;
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Supplier supplier;
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Category category;
+	
+	@Column(name = "unit_price", length = 10)
+	private Integer unitPrice; 
+	
+	@Column(name = "units_in_stock", length = 10)
+	private Integer unitsInStock;
+
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public Supplier getSupplier() {
+		return supplier;
+	}
+
+	public void setSupplier(Supplier supplier) {
+		this.supplier = supplier;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public Integer getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(Integer unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public Integer getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(Integer unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}	
+
+}
