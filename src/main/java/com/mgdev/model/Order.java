5063e58cf9507e79ee1b646b1575254527b85527
ManuelG <manuelguzmanvilches@gmail.com>
2023-05-25 22:01:32 -0400

    [ADD] order model

---
+++ src/main/java/com/mgdev/model/Order.java
@@ -0,0 +1,66 @@
+package com.mgdev.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "order")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id; 
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Customer customer; 
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Employee employee;
+		
+	private LocalDateTime date;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+	
+	
+	
+
+}
